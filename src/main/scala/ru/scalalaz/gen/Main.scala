cd9da5b2d18a829e40d8a48261a50b3e56ce2fad
Evgeniy Tokarev <archjohn@gmail.com>
2016-09-07 10:05:35 +0500

    disqus tag implemented & audio controls has a type optional parameter now

--- src/main/scala/ru/scalalaz/gen/Main.scala
+++ src/main/scala/ru/scalalaz/gen/Main.scala
@@ -16,37 +16,54 @@
 
 package ru.scalalaz.gen
 
+import com.typesafe.config.ConfigFactory
 import java.nio.file.{ Files, Paths }
-
 import laika.api.Transform
-import laika.directive.Directives
 import laika.directive.Directives._
 import laika.parse.markdown.Markdown
-import laika.render.{ HTML, HTMLWriter, PrettyPrint }
-import laika.tree.TreeUtil
-import laika.tree.Documents._
-import laika.tree.Templates.{ TemplateElement, TemplateString }
-import laika.util.Builders._
+import laika.render.{ HTML, PrettyPrint }
 import laika.tree.Elements._
-
-import scala.collection.JavaConversions._
+import laika.util.Builders._
 import scala.io.Codec
 
-/**
-  * Created by Evgeniy Tokarev on 03/09/16.
-  */
 object Main extends App {
 
   implicit val codec: Codec = Codec.UTF8
+
+  val config     = ConfigFactory.load()
+  val disqusCode = config.getString("disqus.disqusCode")
 
   val audioControls: Spans.Directive = Spans.create("audioControls") {
     import Spans.Combinators._
     import Spans.Converters._
 
-    (attribute(Default) ~ body(Default).optional) { (src, content) =>
-      RawContent(Seq("html"), s"""|<audio controls="">
-            |<source src="$src" type="audio/mpeg">
+    (attribute(Default) ~ attribute("type").optional ~ body(Default).optional) {
+      (src, `type`, content) =>
+        val t = `type`.getOrElse("mpeg")
+        RawContent(Seq("html"), s"""|<audio controls="">
+            |<source src="$src" type="audio/$t">
             |</audio>""".stripMargin)
+    }
+  }
+
+  val discuss: Spans.Directive = Spans.create("disqus") {
+    import Spans.Combinators._
+    import Spans.Converters._
+
+    (attribute(Default).optional ~ body(Default).optional) {
+      (value, content) =>
+        val disqusJSCode = """
+        var disqus_shortname = '""" + disqusCode + """';
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();"""
+
+        val html =
+          s"""<div id="disqus_thread"></div><script type="text/javascript">$disqusJSCode</script>"""
+
+        RawContent(Seq("html"), html)
     }
   }
 
@@ -59,14 +76,14 @@ object Main extends App {
   }
 
   // for debugging //
-  val testFile = getClass.getResource("/02.md").getPath()
+  val testFile = getClass.getResource("/02.md").getPath
   print(
       Transform from Markdown
-        .withSpanDirectives(audioControls) to PrettyPrint fromFile testFile toString
+        .withSpanDirectives(discuss, audioControls) to PrettyPrint fromFile testFile toString
   )
   //
 
   // html generation
   Transform from Markdown
-    .withSpanDirectives(audioControls) to HTML fromDirectory resourcesDir toDirectory targetPath.toString
+    .withSpanDirectives(discuss, audioControls) to HTML fromDirectory resourcesDir toDirectory targetPath.toString
 }
