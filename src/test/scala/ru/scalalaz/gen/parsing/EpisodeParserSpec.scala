40b516319b23a4703709aec3e531988ef8bab660
dos65 <qtankle@gmail.com>
2016-12-15 23:13:17 +0300

    missing formatting

--- src/test/scala/ru/scalalaz/gen/parsing/EpisodeParserSpec.scala
+++ src/test/scala/ru/scalalaz/gen/parsing/EpisodeParserSpec.scala
@@ -1,14 +1,29 @@
+/*
+ * Copyright 2016 Scalalaz Podcast Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ru.scalalaz.gen.parsing
 
 import java.time.LocalDate
 
 import cats.data.Validated.Valid
-import org.scalatest.{FlatSpec, Inside, Matchers}
+import org.scalatest.{ FlatSpec, Inside, Matchers }
 
 class EpisodeParserSpec extends FlatSpec with Matchers with Inside {
 
-  val episodeStr =
-    """
+  val episodeStr = """
       |title=Episode#1
       |page=http://scalalaz.ru/series-01.html
       |date=2016-11-28
