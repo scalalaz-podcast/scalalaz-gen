d14139ab0c3ae6881bcc4479f6f19c88fbeb0259
Vadim Chelyshov <qtankle@gmail.com>
2016-12-28 18:34:18 +0300

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "start script"
-set -x
 cd $TRAVIS_BUILD_DIR/target
 tar cvf site_$TRAVIS_BUILD_NUMBER.tar site
-scp site.tar travis@scalalaz.ru:
+scp site_$TRAVIS_BUILD_NUMBER.tar travis@scalalaz.ru:
