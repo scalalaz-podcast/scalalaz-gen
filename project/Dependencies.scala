66137517b08867c7947989dd9ff9807dee93bce1
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-12-05 23:12:37 -0500

    Add more comments about the knockoff dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,9 @@ object Version {
   final val ScalaTags = "0.11.0"
   final val FastParse = "2.3.3"
   final val Cats      = "2.7.0"
-  // https://github.com/playframework/twirl/blob/b2e049d993d1522249e79837ee90ad39f317a80e/build.sbt#L11-L16
+  // both sbt-twirl and knockoff depend on scala-xml 1.x
+  // starting version 0.9 knockoff depends on scala-xml 2.x which makes it impossible for us to use
+  // for the additional context see https://github.com/playframework/twirl/blob/b2e049d993d1522249e79837ee90ad39f317a80e/build.sbt#L11-L16
   final val Knockoff       = "0.8.14"
   final val TypesafeConfig = "1.4.1"
 }
